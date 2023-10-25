0f1ad6c5d2987167bd65c086b7aa67351b7e993c
aleks9045 <aleksey9045@gmail.com>
2023-10-25 15:27:10 +0300

    add is_admin

--- app/auth/models.py
+++ app/auth/models.py
@@ -8,6 +8,7 @@ user = Table(
     Column('id', type_=Integer, primary_key=True),
     Column('email', String, nullable=False, unique=True),
     Column('name', String(16), nullable=False),
+    Column('is_admin', Boolean, nullable=False),
     Column('photo', String, nullable=True),
     Column("hashed_password", String(length=1024), nullable=False),
     Column("is_active", Boolean, default=True, nullable=False),
